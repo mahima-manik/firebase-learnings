3b18264d75b1444018a0e8780d2b16c82ccf2df1
Mahima Manik <mahimamanik.22@gmail.com>
2024-04-14 14:25:08 +0530

    Initialize Firebase app using config

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # firebase-learnings
 NodeJS project covering all important Firebase concepts like Auth, storage, firestore, cloud functions
+
+
+### How to run
+
+```
+npm install
+npm run dev
+```
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello, world!');
+import { initializeApp } from "firebase/app";
+
+// Setup dotenv for environment variables
+import dotenv from 'dotenv';
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log('Firebase initialized!', app);
