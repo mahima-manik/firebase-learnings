0e4ee66812f61f7759ff792061cf244c1a91cfe3
Mahima Manik <mahimamanik.22@gmail.com>
2024-04-14 14:43:30 +0530

    Add auth related code

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import { initializeApp } from "firebase/app";
+import { 
+    getAuth, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword,
+    onAuthStateChanged, 
+    signOut } from "firebase/auth";
 
 // Setup dotenv for environment variables
 import dotenv from 'dotenv';
@@ -15,6 +21,51 @@ const firebaseConfig = {
 };
 
 
-// Initialize Firebase
+/********************* Firebase setup *********************/
+
 const app = initializeApp(firebaseConfig);
-console.log('Firebase initialized!', app);
+console.log('Firebase initialized with app name:', app.options.projectId);
+
+
+/********************* Auth related functions *********************/
+
+// Get the auth service for the current app
+const auth = getAuth(app);
+
+// Create a new user
+const createUser = () => {
+    return createUserWithEmailAndPassword(auth, USER_EMAIL, USER_PASSWORD)
+}
+
+// Sign in an existing user
+const signInUser = async () => {
+    return signInWithEmailAndPassword(auth, USER_EMAIL, USER_PASSWORD)
+}
+
+// Sign out the current user
+const signOutUser = async () => {
+    return signOut(auth);
+}
+
+// Listen to any change in auth user
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        console.log('User is logged in with email:', user.email);
+    } else {
+        console.log('Logged out');
+    }
+});
+
+
+/********************* Main code *********************/
+
+const USER_EMAIL = 'testuser@test.com';
+const USER_PASSWORD = 'testpassword';
+let authUser = null;
+try {
+    authUser = await createUser();
+} catch (error) {
+    console.log('Error creating user:', error.message);
+    authUser = await signInUser();
+    console.log('User signed in with uid:', authUser.user.uid);
+}
